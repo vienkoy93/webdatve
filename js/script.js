3bec0ee40556a2fc582bfc70be1db01e85bc4b0e
TranVietTin <1751010160tin@ou.edu.vn>
2020-06-07 13:03:27 +0700

    3rd

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-$(document).ready(function(){
-  $("button").click(function(){
-    $("p").toggle(1000);
-  });
+$(document).ready(function() {
+		$('.nav-link a').click(function(event) {
+			event.preventDefault();
+			part = $(this).attr('href'); // lấy id của thẻ tương ứng
+			position = $(part).offset().top; // tìm vị trí 
+			$('html, body').animate({scrollTop: position},1000,'easeInOutSine');
+		});
+});
+//tat video modal
+$('#videoModal').on('hidden.bs.modal', function () {
+    $("#videoModal iframe").attr("src", $("#videoModal iframe").attr("src"));
+});
+//
+$("#dates").click(function(){
+  $("#showtimes").show();
 });
